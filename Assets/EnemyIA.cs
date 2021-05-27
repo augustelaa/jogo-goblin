39656759894fe68163f478eb9166f54c2242085f
Augusto <augusto_conceicao@hotmail.com>
2021-05-26 22:11:20 -0300

    Ajustes gerais.

--- Assets/EnemyIA.cs
+++ Assets/EnemyIA.cs
@@ -7,16 +7,11 @@ public class EnemyIA : MonoBehaviour
 {
 	public AIPath aiPath;
 	private Animator anim;
-	private Quaternion lookLeft;
-	private Quaternion lookRight;
-	private float timeCount = 1.0f;
 	
     // Start is called before the first frame update
     void Start()
     {
 		anim = GetComponent<Animator>();
-        lookRight = transform.rotation;
-		lookLeft = lookRight * Quaternion.Euler(0, 180, 0); 
     }
 
     // Update is called once per frame
@@ -33,16 +28,5 @@ public class EnemyIA : MonoBehaviour
 			anim.SetBool ("Walk_Cycle_1", false);
 			anim.SetBool ("Rest_1", true);
 		}
-		
-		
-        if (aiPath.desiredVelocity.x >= 0.01f)
-		{
-			transform.rotation = Quaternion.Slerp(transform.rotation, lookLeft, timeCount);
-		}
-		else if(aiPath.desiredVelocity.x <= 0.01f)
-		{
-			transform.rotation = Quaternion.Slerp(transform.rotation, lookRight, timeCount);
-		}
-		timeCount = timeCount + Time.deltaTime;
     }
 }
