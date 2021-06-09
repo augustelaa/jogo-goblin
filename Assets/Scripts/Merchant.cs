395414afb01492549c0c764c33bf375f7fa1dfeb
Rafael Froeschlin Filho <rafael.froe.filho@gmail.com>
2021-06-09 19:57:17 -0300

    Added basic merchant functionality

---
+++ Assets/Scripts/Merchant.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Merchant : MonoBehaviour
+{
+    public GameObject player;
+    public PlayerController playerController;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        float distance = Vector3.Distance(player.transform.position, gameObject.transform.position);
+        if (Input.GetKey(KeyCode.E) && distance < 5)
+            playerController.bowQuality = "gold";
+    }
+}
