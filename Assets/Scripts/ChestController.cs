22e0c2e4276d54cf498f74c3973c113a37652101
Rafael Froeschlin Filho <rafael.froe.filho@gmail.com>
2021-06-09 21:32:06 -0300

    Added score system and a chest

---
+++ Assets/Scripts/ChestController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChestController : MonoBehaviour
+{
+    public GameObject player;
+    public MeshRenderer coins;
+    public PlayerController playerController;
+    private bool hasGold = true;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        float distance = Vector3.Distance(player.transform.position, gameObject.transform.position);
+        if (Input.GetKeyDown(KeyCode.E) && distance < 5 && hasGold)
+		{
+			playerController.score += 5;
+            hasGold = false;
+            coins.enabled = false;
+		}
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public string bowQuality = "wood";
+    public int score = 0;
     // Start is called before the first frame update
     void Start()
     {
